63345c3e87c2d88c0110ec45064c84e5a7f90491
adermake <adermake@web.de>
2019-06-22 20:02:58 +0200

    Death v2

--- src/esze/listeners/Death.java
+++ src/esze/listeners/Death.java
@@ -3,8 +3,13 @@ package esze.listeners;
 import org.bukkit.Bukkit;
 import org.bukkit.Color;
 import org.bukkit.Particle;
+import org.bukkit.entity.Damageable;
+import org.bukkit.entity.LivingEntity;
+import org.bukkit.entity.Player;
+import org.bukkit.event.Event;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntityDamageEvent;
 import org.bukkit.event.entity.PlayerDeathEvent;
 
 import esze.enums.GameType;
@@ -14,16 +19,21 @@ import esze.utils.ParUtils;
 public class Death implements Listener {
 	
 	@EventHandler
-	public void onDeath(PlayerDeathEvent e){
+	public void onDeath(EntityDamageEvent e){
 		
 			// TEST
-		GameType.getType().death(e);
-			e.getEntity().setHealth(20);
-			Bukkit.broadcastMessage("Hesda"+e.getEntity().getHealth());
-			e.setDeathMessage("s");
+		if (e.getEntity() instanceof Player) {
+			Player p = (Player) e.getEntity();
+			PlayerDeathEvent event = new PlayerDeathEvent(p, null, 0, "he dead");
+			
+			((LivingEntity) e.getEntity()).setHealth(20);
+			
 			
 			
 			ParUtils.createRedstoneParticle(e.getEntity().getLocation(), 0.3, 0.5, 0.3, 10, Color.RED, 3);
+			GameType.getType().death(event);
+		}
+		
 	}
 
 }
